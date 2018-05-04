e2ef46fabb13104a4f9752887b9b6d405f76bb56
yukornienko <36741729+yukornienko@users.noreply.github.com>
2018-05-04 22:06:17 +0300

    Update script.py

--- script.py
+++ script.py
@@ -87,6 +87,58 @@ class Graph:
         #self.G.draw(output_name, format='png', prog='dot')
         self.G.write(output_name)
         
+        def compress (self):
+       
+        self.to_be_comressed = [] #создаем список вершин, которые будем сжимать (первые из них)
+        
+        for node in self.vertices.keys():
+            if len(list(self.vertices[node].out_edges.keys())) == 1:
+                next_node = list(self.vertices[node].out_edges)[0]
+                if len(list(self.vertices[node].in_edges.keys())) == 0 and len(list(self.vertices[next_node].in_edges.keys())) == 1:
+                    self.to_be_comressed += [node]
+                   
+                elif len(list(self.vertices[node].in_edges.keys())) == 1 and len(list(self.vertices[next_node].in_edges.keys())) == 1:
+                    if list(self.vertices[node].in_edges.keys())[0] not in self.to_be_comressed: 
+                        self.to_be_comressed += [node]
+                        
+        for node in self.to_be_comressed:  #иттерируемся по созданному списку
+            self.new_edges = []
+            self.new_edges_in = []
+    
+            next_node = list(self.vertices[node].out_edges)[0]
+            new_node = str(node) + str(next_node)[-1]
+
+            if new_node in self.vertices:
+                self.vertices[new_node].increase_coverage()                
+            else:
+                self.vertices[new_node] = Vertex(new_node)
+                self.vertices[new_node].coverage = self.vertices[node].coverage
+
+                if len(list(self.vertices[node].in_edges)) != 0:
+                    
+                    previous_node = list(self.vertices[node].in_edges)[0]
+                    new_in_edge = Edge(previous_node, new_node)
+                    del self.vertices[previous_node].out_edges[node]
+                    self.vertices[new_node].in_edges[previous_node] = [new_in_edge]
+                    self.vertices[previous_node].out_edges[new_node] = [new_in_edge]
+                    del self.vertices[node].in_edges[previous_node]
+
+                if len(list(self.vertices[next_node].out_edges)) != 0:
+                    for out_edge_ in self.vertices[next_node].out_edges:
+                        self.new_edges += [Edge(new_node, out_edge_)]
+                        del self.vertices[out_edge_].in_edges[next_node]
+                        self.vertices[out_edge_].in_edges[new_node] = self.new_edges 
+                        self.vertices[new_node].out_edges[out_edge_] = self.new_edges
+                    
+                    del self.vertices[next_node].out_edges
+
+            del self.vertices[next_node].in_edges                    
+            del self.vertices[node].out_edges
+            del self.vertices[node]
+            del self.vertices[next_node]
+
+
+        
 k = 15
     
 my_graph = Graph(k)
@@ -106,6 +158,9 @@ with open("/Users/yukornienko/Downloads/BI_spring_2018/Python/hw_4_5_dataset.fas
         for e in my_graph.vertices[v].in_edges:
             print('-> In edge: {}'.format(e))    
   
-my_graph.visualize("short", "/Users/yukornienko/Downloads/BI_spring_2018/Python/HW4_short_15.dot")
-my_graph.visualize("full", "/Users/yukornienko/Downloads/BI_spring_2018/Python/HW4_full_15.dot")
+my_graph.visualize("short", "/HW4_short_15.dot")
+my_graph.visualize("full", "/HW4_full_15.dot")
+my_graph.compress()
+my_graph.visualize("full", "/hw_5_compressed_15full.dot")
+my_graph.visualize("short", "/hw_5_compressed_15short.dot")
 
